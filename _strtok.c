440eb65cd085fafaf06bffe676190e058e72e357
Jbocane6 <jbocane6@gmail.com>
2021-08-16 17:34:04 -0500

    _strtok.c v.1

---
+++ _strtok.c
@@ -0,0 +1,91 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define err STDOUT_FILENO
+
+void _strtok(char *str, const char *delim)
+{
+    char *cpStr, *arg;
+    char **exp;
+    int i = 0, j = 0, size = 0, k, l = 0, args = 1;
+
+    for (i = 0; str[i]; i++)
+    {
+        if (str[i] == *delim && str[i + 1] != '\0')
+            args++;
+    }
+
+    exp = (char **) malloc(sizeof(char) * args);
+    if (!*exp)
+    {
+        printf("ERROR malloc 1");
+        exit(100);
+    }
+
+    cpStr = malloc(sizeof(char) * strlen(str));
+    if (!cpStr)
+    {
+        printf("ERROR malloc 2");
+        exit(100);
+    }
+
+    while (str[j])
+    {
+        if (str[j] != *delim)
+        {
+            cpStr[i] = str[j];
+            i++;
+        }
+        else
+        {
+            exp[l] = (char *) malloc(sizeof(char) * i);
+            if (!exp[l])
+            {
+                printf("ERROR malloc 3");
+                exit(100);
+            }
+            for (k = 0; k < i; k++)
+                exp[l][k] = cpStr[k];
+            l++;
+            i = 0;
+        }
+        j++;
+    }
+    if (i > 0)
+    {
+        exp[l] = (char *) malloc(sizeof(char) * i);
+            if (!exp[l])
+            {
+                printf("ERROR malloc 4");
+                exit(100);
+            }
+            for (k = 0; k < i; k++)
+                exp[l][k] = cpStr[k];;
+            l++;
+            i = 0;
+    }
+    for (k = 0; i < args; k++)
+    {
+        printf("%s\t", (char *)exp[k]);
+    }
+    
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("ERROR");
+        exit(98);
+    }
+
+    if (!argv[1] || !argv[2])
+    {
+        printf("ERROR ARGVS");
+        exit(99);
+    }
+    _strtok(argv[1], argv[2]);
+    return (0);
+}
---
+++ fork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+   fork(); /* A */
+   ( fork()  /* B */ &&
+     fork()  /* C */ ) || /* B and C are grouped according to precedence */
+   fork(); /* D */
+   fork(); /* E */
+
+   printf("forked\n");
+   return 0;
+}
