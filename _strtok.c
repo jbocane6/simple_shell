c76045297fb282b96df333947e897c7a0b8f7900
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-16 21:45:42 -0500

    [FIX] - Fix code, betty style and main deleted

--- _strtok.c
+++ _strtok.c
@@ -5,87 +5,77 @@
 
 #define err STDOUT_FILENO
 
+/**
+ * grid - assing size in malloc and save information of cpStr in exp
+ * @exp: pointer double
+ * @cpStr: Information to copy
+ * @l: Pointer to position of exp
+ * @i: Increment
+ * Return: Without return
+ */
 void grid(char **exp, char *cpStr, int *l, int *i)
 {
-    int k;
-    exp[*l] = malloc(sizeof(char) * (*i + 1));
-    if (!exp[*l])
-    {
-        printf("ERROR malloc 3");
-        exit(100);
-    }
-    for (k = 0; k < *i; k++)
-        exp[*l][k] = cpStr[k];
-    exp[*l][k] = '\0';
-    *l += 1;
-    *i = 0;
+	int k;
+
+	exp[*l] = malloc(sizeof(char) * (*i + 1));
+	if (!exp[*l])
+	{
+		printf("ERROR malloc 3");
+		exit(100);
+	}
+	for (k = 0; k < *i; k++)
+		exp[*l][k] = cpStr[k];
+	exp[*l][k] = '\0';
+	*l += 1;
+	*i = 0;
 }
 
-char ** _strtok(char *str, const char *delim)
+/**
+ * _strtok - split a chain of characters
+ * @str: String to split
+ * @delim: delimitador to use in str
+ * Return: Array bidimentional with strings split
+ */
+char **_strtok(char *str, const char *delim)
 {
-    char *cpStr;
-    char **exp;
-    int i = 0, j = 0, k, l = 0, args = 1;
+	char *cpStr;
+	char **exp;
+	int i = 0, j = 0, k, l = 0, args = 1;
 
-    for (i = 0; str[i]; i++)
-    {
-        if (str[i] == *delim && str[i + 1] != '\0')
-            args++;
-    }
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == *delim && str[i + 1] != '\0')
+			args++;
+	}
 
-    exp = malloc(sizeof(char *) * args + 1);
-    if (!exp)
-    {
-        printf("ERROR malloc 1");
-        exit(100);
-    }
+	exp = malloc(sizeof(char *) * args + 1);
+	if (!exp)
+	{
+		printf("ERROR malloc 1");
+		exit(100);
+	}
 
-    cpStr = malloc(sizeof(char) * strlen(str));
-    if (!cpStr)
-    {
-        printf("ERROR malloc 2");
-        exit(100);
-    }
+	cpStr = malloc(sizeof(char) * strlen(str));
+	if (!cpStr)
+	{
+		printf("ERROR malloc 2");
+		exit(100);
+	}
 
-    i = 0;
-    while (str[j])
-    {
-        if (str[j] != *delim)
-        {
-            cpStr[i] = str[j];
-            i++;
-        }
-        else
-            grid(exp, cpStr, &l, &i);
-        j++;
-    }
-    if (i > 0)
-        grid(exp, cpStr, &l, &i);
-    exp[l] = '\0';
-    return (exp);
-    
-}
-
-int main(int argc, char *argv[])
-{
-    int k;
-    char **exp;
-
-    if (argc != 3)
-    {
-        printf("ERROR");
-        exit(98);
-    }
-
-    if (!argv[1] || !argv[2])
-    {
-        printf("ERROR ARGVS");
-        exit(99);
-    }
-    exp = _strtok(argv[1], argv[2]);
-    for (k = 0; exp[k]; k++)
-    {
-        printf("%s\t", exp[k]);
-    }
-    return (0);
+	i = 0;
+	while (str[j])
+	{
+		if (str[j] != *delim)
+		{
+			cpStr[i] = str[j];
+			i++;
+		}
+		else
+			grid(exp, cpStr, &l, &i);
+		j++;
+	}
+	if (i > 0)
+		grid(exp, cpStr, &l, &i);
+	exp[l] = '\0';
+	return (exp);
 }
